ce490400c471400e0ae113bdee2052ee48d840bf
Thabares <thabares786@gmail.com>
2021-11-24 16:15:50 +0530

    added routing path

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
-     <TopBar />
-     {/* <Home /> */}
-     {/* <Single /> */}
-     {/* <Settings /> */}
-     {/* <Login /> */}
-     <Register />
-     {/* <Write /> */}
-    </>
+    <Router>
+    <TopBar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/register" element={user ? <Home /> : <Register />} />
+      <Route exact path="/login" element={user ? <Home /> : <Login />} />
+      <Route exact path="/write" element={user ? <Write /> : <Register />} />
+      <Route exact path="/settings" element={user ? <Settings /> : <Register />}  />
+      <Route exact path="/post/:postId" element={<Single />} />
+    </Routes>
+    </Router>
   );
 }
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,29 +1,64 @@
-import "./topbar.css"
+import { Link } from "react-router-dom";
+import "./topbar.css";
 
 export default function TopBar() {
-    return (
-        <div className="top">
-            <div className="topLeft">
-            <i className="topIcon fab fa-facebook-square"></i>
-            <i className="topIcon fab fa-twitter-square"></i>
-            <i className="topIcon fab fa-pinterest-square"></i>
-            <i className="topIcon fab fa-instagram-square"></i>
-            </div>
-            <div className="topCenter">
-                <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
-                </ul>
-            </div>
-            <div className="topRight">
-                <img
-                className="topImg"
-                src="https://images.pexels.com/photos/9443322/pexels-photo-9443322.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
-                <i className="topSearchIcon fas fa-search"></i>
-            </div>
-        </div>
-    )
+  const user = false;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link to="/" className="link">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/about" className="link">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/contact" className="link">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/write" className="link">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && "LOGOUT"}</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img
+            className="topImg"
+            src="https://images.pexels.com/photos/9443322/pexels-photo-9443322.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+            <Link className="link" to="/register">
+              REGISTER
+            </Link>
+            </li>           
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
 }
